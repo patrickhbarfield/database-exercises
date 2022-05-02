902b2533ec4a12cb9d9df129ea61c0be0677c14a
Pat Barfield <patrickhbarfield@gmail.com>
2022-05-02 16:00:05 -0500

    added caption to SQL query outputs

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,5 +1,27 @@
 USE codeup_test_db;
 
-SELECT name
+SELECT name AS 'Here are all albums by Pink Floyd w/over 30mil sales'
 FROM albums
 WHERE artist = 'Pink Floyd';
+
+
+SELECT release_date AS 'Year Sgt. Pepper''s Lonely Hearts Club Band was released'
+FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre AS 'Genre for the album Nevermind'
+FROM albums
+WHERE name = 'Nevermind';
+
+SELECT name AS 'Albums that were released in the 1990s'
+FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT name AS 'Albums that had less than 20 million certified sales'
+FROM albums
+WHERE sales < 20;
+
+SELECT name AS 'Albums with a genre of "Rock'
+FROM albums
+WHERE genre = 'Rock';
+
