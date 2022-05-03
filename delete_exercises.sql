069224eb9baa889820009306b52ed7396abf8cfc
Pat Barfield <patrickhbarfield@gmail.com>
2022-05-03 12:02:20 -0500

    sql ex walkthrough

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -6,5 +6,11 @@ WHERE release_date >1991;
 DELETE FROM albums
 WHERE genre = 'disco';
 
+# SELECT * FROM albums a WHERE a.genre LIKE '%disco%';
+# don't use * when deleting'
+# DELETE FROM albums a WHERE a.genre LIKE '%disco%';
+
+
 DELETE FROM albums
 WHERE artist = 'Whitney Houston';
+# SELECT * FROM albums a WHERE a.artist = 'Whitney Houston';
--- select_exercises.sql
+++ select_exercises.sql
@@ -1,5 +1,11 @@
 USE codeup_test_db;
 
+SELECT 'Albums by Pink Floyd';
+SELECT * FROM albums WHERE artist LIKE 'P%';
+
+# // creating an ilias is useful if column names have th esame something...
+# SELET * FROM albums a WHERE a.name LIKE 'Sgt. Pepper%';
+
 SELECT name AS 'Here are all albums by Pink Floyd w/over 30mil sales'
 FROM albums
 WHERE artist = 'Pink Floyd';
@@ -13,6 +19,9 @@ SELECT genre AS 'Genre for the album Nevermind'
 FROM albums
 WHERE name = 'Nevermind';
 
+# //this is a quality comparison, vs. a between.
+# SELECT * FROM albums a WHERE a.release_date > 1989 AND a.release_date < 2000;
+
 SELECT name AS 'Albums that were released in the 1990s'
 FROM albums
 WHERE release_date BETWEEN 1990 AND 1999;
@@ -25,3 +34,4 @@ SELECT name AS 'Albums with a genre of "Rock'
 FROM albums
 WHERE genre = 'Rock';
 
+SELECT * FROM albums a WHERE genre = 'rock';
